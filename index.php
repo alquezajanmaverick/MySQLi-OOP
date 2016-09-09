3cc5458904b82a4c1ba72bfd87fbe75a31bc9482
Jan Maverick Alqueza <alquezajanmaverick@gmail.com>
2016-09-09 16:50:58 +0800

    added files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+require('connect/db.inc.php');
+$db = new DatabaseConnect();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Sample MySQLi</title>
+</head>
+
+<body>
+</body>
+</html>
